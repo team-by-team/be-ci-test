a526aac9e51fc38ec5eed1172811d73010e6b0dd
pilyang <pilyang.dev@gmail.com>
2023-07-18 15:12:03 +0900

    test8

--- backend/src/test/java/team/teamby/teambyteam/teamplace/domain/Test.java
+++ backend/src/test/java/team/teamby/teambyteam/teamplace/domain/Test.java
@@ -2,6 +2,8 @@ package team.teamby.teambyteam.teamplace.domain;
 
 import org.junit.jupiter.api.DisplayName;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Test {
 
     @org.junit.jupiter.api.Test
@@ -16,5 +18,18 @@ public class Test {
         System.out.println("cool");
         System.out.println("언제 끝ㅈ날까아ㅏ....");
         System.out.println("hhh");
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("")
+    void wrong() {
+        // given
+        final int num = 1;
+
+        // when
+        assertThat(num).isEqualTo(2);
+
+        //then
+
     }
 }
