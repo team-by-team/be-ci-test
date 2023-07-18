238a2d44a149e4bcc69464409dd84bf49f55f877
pilyang <pilyang.dev@gmail.com>
2023-07-18 16:10:36 +0900

    test: Test class name change

--- backend/src/test/java/team/teamby/teambyteam/teamplace/domain/Test1.java
+++ backend/src/test/java/team/teamby/teambyteam/teamplace/domain/Test1.java
@@ -1,22 +1,31 @@
 package team.teamby.teambyteam.teamplace.domain;
 
 import org.junit.jupiter.api.DisplayName;
+// sksksksk
+// sksksksk
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class Test {
+public class Test1 {
 
-    @org.junit.jupiter.api.Test
+    @Test
     @DisplayName("")
     void test() {
+        // given
+
+        //when
+
+        //then
         System.out.println("Wow good~~~");
         System.out.println("cool");
         System.out.println("언제 끝ㅈ날까아ㅏ....");
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     @DisplayName("")
     void wrong() {
+        // sksksksk
         final int num = 1;
 
         assertThat(num).isEqualTo(2);
@@ -24,13 +33,28 @@ public class Test {
 
     }
 
-    @org.junit.jupiter.api.Test
+    // sksksksk
+    // sksksksk
+    @Test
+    // sksksksk
+    // sksksksk
+    // sksksksk
     @DisplayName("")
+    // sksksksk
+    // sksksksk
     void good() {
         final int num = 1;
 
+        // sksksksk
+        // sksksksk
+        // sksksksk
         assertThat(num).isEqualTo(1);
 
 
+        // sksksksk
+        // sksksksk
+        // sksksksk
+        // sksksksk
+        // sksksksk
     }
 }
