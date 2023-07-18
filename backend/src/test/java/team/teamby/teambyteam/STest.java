ef31d2194acb5073d621470310602954080bab47
sh111-coder <ohk9134@naver.com>
2023-07-18 17:58:16 +0900

    test1

---
+++ backend/src/test/java/team/teamby/teambyteam/STest.java
@@ -0,0 +1,17 @@
+package team.teamby.teambyteam;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class STest {
+
+    @Test
+    @DisplayName("tt")
+    void aa() {
+        // given
+
+        // when
+
+        // then
+    }
+}
