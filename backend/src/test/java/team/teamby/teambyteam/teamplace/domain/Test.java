af6aff8d3c482832529f1a98228ce1e9cb23b011
pilyang <pilyang.dev@gmail.com>
2023-07-18 14:23:09 +0900

    test: test1

---
+++ backend/src/test/java/team/teamby/teambyteam/teamplace/domain/Test.java
@@ -0,0 +1,18 @@
+package team.teamby.teambyteam.teamplace.domain;
+
+import org.junit.jupiter.api.DisplayName;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("")
+    void test() {
+        // given
+
+        // when
+
+        //then
+        System.out.println("Wow good~~~");
+
+    }
+}
